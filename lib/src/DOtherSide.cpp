cab7abc21bc5174d60d040fe9831363a7e172616
Richard Ramos <info@richardramos.me>
2020-06-23 18:48:24 -0400

    feat: allow setting app icon

--- lib/src/DOtherSide.cpp
+++ lib/src/DOtherSide.cpp
@@ -26,6 +26,7 @@
 #include <QtCore/QPointer>
 #include <QtCore/QResource>
 #include <QtGui/QGuiApplication>
+#include <QtGui/QIcon>
 #include <QtQml/QQmlContext>
 #include <QtCore>
 #include <QtQml/QQmlApplicationEngine>
@@ -118,6 +119,11 @@ void dos_qapplication_delete()
 void dos_qapplication_exec()
 {
     qApp->exec();
+}
+
+void dos_qapplication_icon(const char *filename)
+{
+    qApp->setWindowIcon(QIcon(filename));
 }
 
 void dos_qapplication_quit()
