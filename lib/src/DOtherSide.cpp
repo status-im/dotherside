f8fbbcd3dd124f26475e5d0725dcbadfca830257
Richard Ramos <info@richardramos.me>
2020-06-25 10:33:02 -0400

    feat: appname

--- lib/src/DOtherSide.cpp
+++ lib/src/DOtherSide.cpp
@@ -100,11 +100,10 @@ void dos_qguiapplication_quit()
     qGuiApp->quit();
 }
 
-void dos_qapplication_create()
+void dos_qapplication_create(char *appName)
 {
     static int argc = 1;
-    static char empty[1] = {0};
-    static char *argv[] = {empty};
+    static char *argv[] = {appName};
 
     register_meta_types();
 
