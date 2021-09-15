1feaee0b62204747f9a6cc617555971fbeec8c9f
Anthony Laibe <anthony@laibe.cc>
2021-09-15 14:14:42 +0200

    feat: expose to local file

--- lib/src/DOtherSide.cpp
+++ lib/src/DOtherSide.cpp
@@ -1341,3 +1341,8 @@ void dos_statusosnotification_delete(DosStatusOSNotificationObject* vptr)
     if(qobject)
         qobject->deleteLater();
 }
+
+char* dos_to_local_file(const char* filePath)
+{
+    return convert_to_cstring(QUrl(QString::fromUtf8(filePath)).toLocalFile());
+}
