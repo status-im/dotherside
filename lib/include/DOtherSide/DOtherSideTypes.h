ab91e0b35c797b925898bc4c6c3cee399240d2f5
Sale Djenic <aleksandardjenic@status.im>
2022-03-17 17:30:36 +0100

    feat: url scheme event added

--- lib/include/DOtherSide/DOtherSideTypes.h
+++ lib/include/DOtherSide/DOtherSideTypes.h
@@ -101,7 +101,7 @@ typedef void DosQMetaObjectConnection;
 typedef void DosSingleInstance;
 
 /// A pointer to a event object which is actualy a QObject
-typedef void DosEvent;
+typedef DosQObject DosEvent;
 
 /// A pointer to a os notification object which is actualy a QObject
 typedef DosQObject DosOSNotification;
