2982550b5684eddfca84bd7c9fd502ad58dcb7e4
Sale Djenic <aleksandardjenic@status.im>
2022-03-17 17:30:36 +0100

    feat: url scheme event added

--- lib/src/DOtherSide.cpp
+++ lib/src/DOtherSide.cpp
@@ -66,6 +66,7 @@
 
 #include "DOtherSide/Status/DockShowAppEvent.h"
 #include "DOtherSide/Status/OSThemeEvent.h"
+#include "DOtherSide/Status/UrlSchemeEvent.h"
 #include "DOtherSide/Status/OSNotification.h"
 #include "DOtherSide/Status/KeychainManager.h"
 #include "DOtherSide/Status/SoundManager.h"
@@ -1352,6 +1353,11 @@ bool dos_singleinstance_isfirst(DosSingleInstance *vptr)
 {
     auto engine = static_cast<QQmlApplicationEngine*>(vptr);
     return new Status::OSThemeEvent(engine);
+}
+
+::DosEvent* dos_event_create_urlSchemeEvent()
+{
+    return new Status::UrlSchemeEvent();
 }
 
 void dos_event_delete(DosEvent* vptr)
