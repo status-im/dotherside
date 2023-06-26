0fd35c377cf6e8fc1b7763ce8608dc7ac4dde81d
Alex Jbanca <alexjb@status.im>
2023-06-26 14:40:38 +0300

    fix(DOtherSide): Crash when a QSortFilterModel is collected by nim GC
    QSortFilterModel can be used as sourceModel in proxy models. Proxy models rely on receiving the `QObject::destroyed` event when the source model is destroyed to clean the internal data.
    Calling `QObject::disconnect` will also disconnect any connections to the `QObject::destroyed` event leaving dangling pointers in the proxy models.

--- lib/src/DOtherSide.cpp
+++ lib/src/DOtherSide.cpp
@@ -770,7 +770,6 @@ void dos_qvariant_setArray(::DosQVariant *vptr, int size, ::DosQVariant **array)
 void dos_qobject_delete(::DosQObject *vptr)
 {
     auto qobject = static_cast<QObject *>(vptr);
-    qobject->disconnect();
     delete qobject;
 }
 
