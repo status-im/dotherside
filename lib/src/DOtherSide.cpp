fe6ca7cfd6d1c7623143ea8ff630fa2139b38bfa
Khushboo Mehta <mehtakhushboo@Hotmail.com>
2025-11-03 20:22:10 +0530

    chore: Remove keychian services from Dotherside in favor of the one on StatsuQ side

--- lib/src/DOtherSide.cpp
+++ lib/src/DOtherSide.cpp
@@ -62,7 +62,6 @@
 
 #include "DOtherSide/Status/UrlSchemeEvent.h"
 #include "DOtherSide/Status/OSNotification.h"
-#include "DOtherSide/Status/KeychainManager.h"
 #include "DOtherSide/Status/AppDelegate.h"
 
 #ifdef MONITORING
@@ -1587,58 +1586,6 @@ void dos_qsettings_end_group(DosQSettings* vptr)
 }
 
 #pragma endregion
-
-#pragma region KeychainManager
-DosKeychainManager* dos_keychainmanager_create(const char* service,
-    const char* authenticationReason)
-{
-    return new Status::KeychainManager(QString(service), QString(authenticationReason));
-}
-
-char* dos_keychainmanager_read_data_sync(DosKeychainManager* vptr,
-    const char* key)
-{
-    auto obj = static_cast<Status::KeychainManager*>(vptr);
-    if(obj)
-    {
-        return convert_to_cstring(obj->readDataSync(QString(key)));
-    }
-
-    return convert_to_cstring(QString());
-}
-
-void dos_keychainmanager_read_data_async(DosKeychainManager* vptr,
-    const char* key)
-{
-    auto obj = static_cast<Status::KeychainManager*>(vptr);
-    if(obj)
-        obj->readDataAsync(QString(key));
-}
-
-void dos_keychainmanager_store_data_async(DosKeychainManager* vptr,
-    const char* key, const char* data)
-{
-    auto obj = static_cast<Status::KeychainManager*>(vptr);
-    if(obj)
-    {
-        obj->storeDataAsync(QString(key), QString(data));
-    }
-}
-
-void dos_keychainmanager_delete_data_async(DosKeychainManager* vptr,
-    const char* key)
-{
-    auto obj = static_cast<Status::KeychainManager*>(vptr);
-    if(obj)
-        obj->deleteDataAsync(QString(key));
-}
-
-void dos_keychainmanager_delete(DosKeychainManager* vptr)
-{
-    auto qobject = static_cast<QObject*>(vptr);
-    if(qobject)
-        qobject->deleteLater();
-}
 
 #pragma region QTimer
 
